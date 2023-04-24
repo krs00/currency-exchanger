8c82e16c248a572ff00f7f27085a467625141529
Kymani Stephens <kymanist00@gmail.com>
2023-04-24 00:38:27 -0700

    add static method with error handling

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,5 +1,16 @@
 export default class Exchange {
     static getExchange() {
         return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+        .then(function(response) {
+            if (!response.ok) {
+                const errorMsg = `${response.status}, something went wrong!!`
+                throw new Error(errorMsg);
+            } else {
+                return response.json();
+            } 
+        })
+        .catch(function(error) {
+            return error 
+        })
     }
 }
