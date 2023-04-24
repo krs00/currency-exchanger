f31968a773d5932b4ba5629832288431e93c4e41
Kymani Stephens <kymanist00@gmail.com>
2023-04-23 19:18:58 -0700

    remove parseFloat

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@
 // GETS USER INPUTTED USD AMOUNT
 function handleSubmit(event) {
   event.preventDefault();
-  const usdAmount = parseFloat(document.getElementById("usd-amt").value) 
+  const usdAmount = document.getElementById("usd-amt").value;
+  console.log(usdAmount)
 }
 
 // WHEN PAGE LOADS, CALLBACK WILL GRAB FORM ADDING EVENT LISTENER FOR handleSubmit
