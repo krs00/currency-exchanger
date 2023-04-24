0ddc824a30f93968a55215454a00754ff5461c1a
Kymani Stephens <kymanist00@gmail.com>
2023-04-24 00:17:02 -0700

    delete restructuring project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-// FUNCTION WILL POPULATE DROPDOWN MENU WITH POSSIBLE CURRENCY SELECTIONS
-function populateDropdown(response) {
-
-  console.log(response) 
-
-  // const menu = document.getElementById('exchange-currency') 
-
-  // for (let i = 0; i < Object.keys(response.conversion_rates.length); i++ ) {
-
-  //   const dropItem = document.createElement('option');
-  //   menu.appendChild(dropItem);
-
-  // }
-
-}
 
 
 // GETS USER INPUTTED USD AMOUNT
@@ -26,14 +11,7 @@ function handleSubmit(event) {
 window.addEventListener("load", function () {
   this.document.getElementById("form").addEventListener("submit", handleSubmit);
 });
-
+ 
 /* I'm thinking make an API call immediately when the page loads
    so I have data to populate my dropdown menu with!!!! */
-window.addEventListener("load", function () {
-  this.fetch(
-    `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`
-  )
-    .then(function(response) {
-      return response.json();
-    })
-});
+
