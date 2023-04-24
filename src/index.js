f4765194c100ea32c98e99f4a92274a7bf016378
Kymani Stephens <kymanist00@gmail.com>
2023-04-24 02:04:26 -0700

    add function to populate drop down

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
+import Exchange from "./js/exchange";
 
+function populateDropdown() {
+  let promise = Exchange.getExchange();
+  promise.then(function (response) {
+    
+    const rates = response.conversion_rates
+    const selectBox = document.getElementById('exchange-currency')
+    console.log(selectBox)
+
+    for (let i = 0; i < rates[0].length; i++) {
+      const selectOption = document.createElement('option') 
+      selectOption.innerText = 'hi'
+      selectBox.appendChild(selectOption) 
+    }
+
+  });
+}
+
+populateDropdown();
 
 // GETS USER INPUTTED USD AMOUNT
 function handleSubmit(event) {
@@ -11,7 +30,6 @@ function handleSubmit(event) {
 window.addEventListener("load", function () {
   this.document.getElementById("form").addEventListener("submit", handleSubmit);
 });
- 
+
 /* I'm thinking make an API call immediately when the page loads
    so I have data to populate my dropdown menu with!!!! */
-
